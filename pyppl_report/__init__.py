952e22a0229bf9bb7cdd153c093ea310f6d0c17b
pwwang <pwwang@pwwang.com>
2019-08-21 16:46:31 -0500

    0.2.0

--- pyppl_report/__init__.py
+++ pyppl_report/__init__.py
@@ -9,7 +9,7 @@ from pyppl.exception import ProcAttributeError
 from cmdy import CmdyReturnCodeException
 from .report import Report
 
-__version__ = "0.1.5"
+__version__ = "0.2.0"
 
 @hookimpl
 def setup(config):
